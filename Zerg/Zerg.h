3d49d7e83aec92a08066d6eb5e5ac6958a98496a
shuailiu-met <58299241+shuailiu-met@users.noreply.github.com>
2019-12-05 20:28:20 +0100

    Add files via upload

--- Zerg/Zerg.h
+++ Zerg/Zerg.h
@@ -1,16 +1,13 @@
 #pragma once
 #include <list>
-#include <tuple>
 #include <string>
 #include <iostream>
-#include <functional>
-#include <string>
-#include <sstream>
-#include <vector>
+#include <algorithm>
 
 #ifndef ZERG_H_INCLUDED
 #define ZERG_H_INCLUDED
 
+using namespace std;
 //default values for Zerg
 int workers_start = 12;
 int basis_start = 1;
@@ -21,12 +18,12 @@ int vespene_start = 0;
 double mineral_harvesting = 0.7;
 double vespene_harvesting = 0.63;
 double energy_regen_rate = 0.5625;
-double chronoboost_energy_cost = 50;
-double chronoboost_speedup = 1.5;
-int chronoboost_duration = 20;
-double mule_energy_cost = 50;
-int mule_duration = 64;
-double mule_speedup = 3.8;
+//double chronoboost_energy_cost = 50;
+//double chronoboost_speedup = 1.5;
+//int chronoboost_duration = 20;
+//double mule_energy_cost = 50;
+//int mule_duration = 64;
+//double mule_speedup = 3.8;
 double injectlarvae_energy_cost = 25;
 int injectlarvae_duration = 29;
 int injectlarvae_amount = 3;
@@ -39,6 +36,14 @@ int max_larvae_per_building = 3;
 //producer_occupied - 2;
 //for pair
 //use list
+
+int minerals = 50;
+int vespene = 0;
+int supply_occupied_initial = 12;//8+6-12
+int supply_limit_initial = 14;
+int num_larvae = 3;
+int num_worker = 12;
+
 
 class Zerg
 {
@@ -56,58 +61,97 @@ class Zerg
         int start_energy;
         int max_energy;
         bool structure;//false for unit,true for building
-        Zerg *test;
+        string status;
 
-        static int minerals;
+        /*static int minerals;
         static int vespene;
         static int supplyremaining;
         static int num_larvae;
-        static int num_worker;
-
-        virtual Zerg() = 0;
-        bool checklarvae();
-        bool checkworker();
-        bool checksupply();
-        bool checkdependency();
-        bool checkmineral();
-        bool checkgas();
+        static int num_worker;*/
+        Zerg(string,string,int,string);
+        //bool dependencycheck();
+        //bool check(Zerg &);
+        //bool operator()(const Zerg &) const;
+        //bool checklarvae();
+        //bool checkworker();//use workerlist.length();
+        //bool checksupply();
+        //bool checkdependency();
 };
-
-bool Zerg::checkgas()
+  // use find_if func
+list<Zerg> ZL;
+Zerg::Zerg(string n,string d,int s,string st):type(n),dependency(d),supply_cost(s),status(st)
 {
-    return true;
+    cout << "Zerg" << endl;
 }
 
-bool Zerg::checkmineral()
+/*bool check(Zerg &z)
 {
+    return (this->dependency == z.type);
+}*/
+
+/*bool dependencycheck(string d)
+{
+    auto it = find_if(ZL.begin(),ZL.end(),[d](const Zerg& zerg) { return zerg.type == d;});
+    cout<< it->dependency;
     return true;
+    else
+    {
+        return false;
+        cout << "f";
+    }
 }
 
-bool Zerg::checksupply()
+void Addontolist(Zerg &z)
 {
-    return true;
+if(dependencycheck(z.dependency)==true)
+    {
+        //num_worker = num_worker-1;
+        ZL.push_back(z);
+        cout << "Built";
+}
+else
+{
+    cout << "f";
+}
+}*/
+
+void Addontolist(Zerg &z)
+{
+    if(z.dependency=="none")
+    {
+        ZL.push_back(z);
+    }
+    else{
+    string target[2] = {z.dependency,"free"};
+    auto it = find_if(ZL.begin(),ZL.end(),[&target](const Zerg &z){return (z.type == target[0])&&(z.status == target[1]);});
+    cout << it->supply_cost <<endl;
+    ZL.push_back(z);
+    }
 }
 
-bool Zerg::checkdependency()
-{
-    return true;
-}
 
-bool Zerg::checkworker()
-{
-    return true;
-}
 
-bool Zerg::checklarvae()
-{
-    return true;
-}
 
-int Zerg::minerals = 50;
-int Zerg::vespene = 0;
-int Zerg::supplyremaining = 2;//8+6-12
-int Zerg::num_larvae = 3;
-int Zerg::num_worker = 12;
+
+
+/*class Worker:protected Zerg
+{
+    private:
+        bool harvest_status;//0 for gas,1 for minerals
+    public:
+        status();
+        {
+            return harvest_status;
+        }
+
+};
+list<Worker> workerlist;
+
+
+
+
+
+
 
 class Building:protected Zerg
 {
@@ -117,48 +161,54 @@ class Building:protected Zerg
 
     public://read info from CSV file required in this class
         Building()
-        {
-
-        };
-
+        {};
         Buildingcheck()
         {
-            if((checkdependency()==true)&&(checkworker()==true)){
-
-            }
             return true;
         };
-        Addontolist(Building &);
+
 };
 
 list<Building> BL;
 
-Building::Addontolist(Building &b)
+void Addontolist(Building &b)
 {
 if(b.Buildingcheck()==true)
     {
         num_worker = num_worker-1;
         BL.push_front(b);
+        cout << "Built";
 }
 }
+
+
+
+
 
 class Unit:protected Zerg
 {
     private:
 
     public:
-        Unit()
+        Unit(){}
+        Unitcheck()
         {
-            if((checkdependency()==1)&&(checklarvae()==1)&&(checksupply()==1)){
-
-            }
-            num_larvae = num_larvae - 1;
+            return true;
         }
 
 };
 
 list<Unit> UL;
 
+void Addontolist(Unit &u)
+{
+if(u.Unitcheck()==true)
+{
+    num_larvae = num_larvae-1;
+    UL.push_front(u);
+    cout << "produced";
+}
+}
 
 
 
@@ -170,5 +220,5 @@ class Queen:protected Unit
 
 };
 
-list<Queen> QL;
+list<Queen> QL;*/
 #endif // ZERG_H_INCLUDED
