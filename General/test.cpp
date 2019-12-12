58f94e816374c4fe758d5144d025baefd90f5a3f
shuailiu-met <58299241+shuailiu-met@users.noreply.github.com>
2019-12-12 16:00:01 +0100

    NewTest of BuildingsAndUnits including initializer
    Now input racename and it will initialize a list with original 12 workers and a base. Then we can build new objects by input a string.

--- General/test.cpp
+++ General/test.cpp
@@ -9,15 +9,80 @@
 
 using namespace std;
 
+//Initialize the Buildings and Units which is existed at beginning, put them into the list
+void Initializer(string race)
+{
+    if(race == "Zerg")
+    {
+    BuildingsAndUnits *b = new BuildingsAndUnits("Hatchery");
+    b->Initial_Set_OnTo_List();
+    b = new BuildingsAndUnits("Overload");
+    b->Initial_Set_OnTo_List();
+    for(int i = 0;i<12;i++)
+    {
+    b = new BuildingsAndUnits("Drone");
+    b->Initial_Set_OnTo_List();
+    }
+    }
+
+
+    else if(race == "Terran")
+    {
+    BuildingsAndUnits *b = new BuildingsAndUnits("CommandCenter");
+    b->Initial_Set_OnTo_List();
+    for(int i = 0;i<12;i++)
+    {
+    b = new BuildingsAndUnits("SCV");
+    b->Initial_Set_OnTo_List();
+    }
+    }
+
+
+    else if(race == "Protoss")
+    {
+    BuildingsAndUnits *b = new BuildingsAndUnits("Nexus");
+    b->Initial_Set_OnTo_List();
+    for(int i = 0;i<12;i++)
+    {
+    b = new BuildingsAndUnits("Probe");
+    b->Initial_Set_OnTo_List();
+    }
+    }
+
+    else
+    {
+        cout << "Please input correctly" << endl;
+    }
+}
+
+//Build a new Unit, if it can be put into the list(the function will return 1), then put. If the push failed, then delete the object.
+void Build(string i)
+{
+    BuildingsAndUnits *b = new BuildingsAndUnits(i);
+    bool success = b->Push_And_Check();
+    if(success==false)
+    {
+        delete b;
+    }
+}
+
 int main()
 {
-//We can use cin or read from buildinglist to get the name.
-//Here in the test I set resource sufficient to test the producer and dependency
-    BuildingsAndUnits *b = new BuildingsAndUnits("Drone");
-    b->Addontolist();
-    b = new BuildingsAndUnits("Hatchery");
-    b->Addontolist();
-    b = new BuildingsAndUnits("SpawningPool");
-    b->Addontolist();
+    //We can use cin or read from buildinglist to get the name.
+    //Here in the test I set resource sufficient to test the producer and dependency
+    string race;
+    cout << "Choose your Race:";
+    cin >> race;
+    Initializer(race);//Initializing
+    string i;
+    while(i!="end")
+    {
+    cin >> i;
+    Build(i);//Build new object
+    }
+
     return 0;
 }
+
+
+
