5a87c642faffd0157cb51a4171254ff718337c29
shuailiu-met <58299241+shuailiu-met@users.noreply.github.com>
2019-12-12 01:30:16 +0100

    test for BuildingsAndUnits class
    set high resource to validating procedure

---
+++ General/test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+
+
+#include "DataAcc.h"
+#include "BuildingsAndUnits.h"
+
+using namespace std;
+
+int main()
+{
+//We can use cin or read from buildinglist to get the name.
+//Here in the test I set resource sufficient to test the producer and dependency
+    BuildingsAndUnits *b = new BuildingsAndUnits("Drone");
+    b->Addontolist();
+    b = new BuildingsAndUnits("Hatchery");
+    b->Addontolist();
+    b = new BuildingsAndUnits("SpawningPool");
+    b->Addontolist();
+    return 0;
+}
