2e05dc1258f1321fa75664603369e5efbc0a8e32
shuailiu-met <58299241+shuailiu-met@users.noreply.github.com>
2019-12-14 19:25:28 +0100

    Worker calculation, harvest update and initialize
    I think the harvest update could be later put into the Race class since there's no difference on that.
    
    TO DO:
    Harvest rate/minerals/vespene data type need to be considered

--- Zerg/Zerg.h
+++ Zerg/Zerg.h
@@ -7,9 +7,13 @@
 Class Zerg: public Race{
 private:
 public:
+    Zerg(std::vector<std::string> buildorder) : Race{buildorder}{}
     int workersOnVespene();
+    void Initializer();
+    void harvestupdate();
 }
 
+//it will return the num of worker on gas
 int Zerg::workersOnVespene()
 {
     string target = "Extractor";
@@ -27,6 +31,33 @@ int Zerg::workersOnVespene()
     return i;
 }
 
+
+//I think this function should be called in the constructor,when we create the race object
+void Zerg::Initializer()
+{
+    Units u("Hatchery",0);
+    finished.push_back(u);
+    Units o("Overload",0);
+    finished.push_back(o);
+    Units worker[12]("Drone",0);
+    for(int i = 0;i<12;i++)
+    {
+        finished.push_back(worker[i]);
+    }
+}
+
+//I think this function can be merged into the advanceOneTimeStep and this function could be put in the race class
+void Zerg::harvestupdate()
+{
+    int worker_vespene = workersOnVespene();
+    int worker_mineral = workers - worker_vespene;
+    mineral_harvesting = data->getParameter("MINERAL_HARVESTING");
+    vespene_harvesting = data->getParameter("VESPENE_HARVESTING");
+    double add_mineral = mineral_harvesting*worker_mineral;//here the data type need to be considered.
+    double add_vespene = vespene_harvesting*worker_vespene;
+    minerals += add_mineral;
+    vespene += add_vespene;
+}
 
 
 #endif // ZERG_H_INCLUDED
