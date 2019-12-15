810a3f6c0601b6688ac70c28df5880499cfbcc88
shuailiu-met <58299241+shuailiu-met@users.noreply.github.com>
2019-12-15 16:02:02 +0100

    Add files via upload

---
+++ Zerg/Zerg.cpp
@@ -0,0 +1,49 @@
+#include "Zerg.h"
+#include <iostream>
+#include <string>
+
+//it will return the num of worker on gas
+void distributeWorkers();
+{
+    string target = "Extractor";
+    auto it = finished.begin();
+    int i = 0;
+    while(it!=finished.end())
+    {
+    it = find_if(it,finished.end(),[&target](const Unit &u){return (u.getName() == target);});
+    if(it!=finished.end())
+    {
+        i++;
+        it++;
+    }
+    }
+    worker_vespene = i*3;
+    worker_minerals = workers - worker_vespene;
+}
+
+
+//I think this function should be called in the constructor,when we create the race object
+void Zerg::Initializer()
+{
+    //here don't use the getUnit() because here the building time should be set to 0
+    Units u("Hatchery",0,1,false);
+    finished.push_back(u);
+    Units o("Overload",0,1,true);
+    finished.push_back(o);
+    Units worker[12]("Drone",0,1,true);
+    for(int i = 0;i<12;i++)
+    {
+        finished.push_back(worker[i]);
+    }
+}
+
+//I think this function can be merged into the advanceOneTimeStep and this function could be put in the race class
+void Zerg::updateResources()
+{
+    mineral_harvesting = data->getParameter("MINERAL_HARVESTING", true);
+    vespene_harvesting = data->getParameter("VESPENE_HARVESTING", true);
+    int add_mineral = mineral_harvesting*worker_mineral;//here the data type need to be considered.
+    int add_vespene = vespene_harvesting*worker_vespene;
+    minerals += add_mineral;
+    vespene += add_vespene;
+}
--- Zerg/Zerg.h
+++ Zerg/Zerg.h
@@ -6,58 +6,16 @@
 
 Class Zerg: public Race{
 private:
+    int energy;
+    int larva_injection_time;
 public:
     Zerg(std::vector<std::string> buildorder) : Race{buildorder}{}
-    int workersOnVespene();
+    void distributeWorkers();
     void Initializer();
-    void harvestupdate();
+    void updateResources();
+    void advanceBuildingProcess();
+    void specialAbility();
+    int startBuildingProcess();
 }
-
-//it will return the num of worker on gas
-int Zerg::workersOnVespene()
-{
-    string target = "Extractor";
-    auto it = finished.begin();
-    int i = 0;
-    while(it!=finished.end())
-    {
-    it = find_if(it,finished.end(),[&target](const Unit &u){return (u.getName() == target);});
-    if(it!=finished.end())
-    {
-        i++;
-        it++;
-    }
-    }
-    return i;
-}
-
-
-//I think this function should be called in the constructor,when we create the race object
-void Zerg::Initializer()
-{
-    Units u("Hatchery",0);
-    finished.push_back(u);
-    Units o("Overload",0);
-    finished.push_back(o);
-    Units worker[12]("Drone",0);
-    for(int i = 0;i<12;i++)
-    {
-        finished.push_back(worker[i]);
-    }
-}
-
-//I think this function can be merged into the advanceOneTimeStep and this function could be put in the race class
-void Zerg::harvestupdate()
-{
-    int worker_vespene = workersOnVespene();
-    int worker_mineral = workers - worker_vespene;
-    mineral_harvesting = data->getParameter("MINERAL_HARVESTING");
-    vespene_harvesting = data->getParameter("VESPENE_HARVESTING");
-    double add_mineral = mineral_harvesting*worker_mineral;//here the data type need to be considered.
-    double add_vespene = vespene_harvesting*worker_vespene;
-    minerals += add_mineral;
-    vespene += add_vespene;
-}
-
 
 #endif // ZERG_H_INCLUDED
